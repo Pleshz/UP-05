60bb94ce331501373dbffc6451f01c771b84d308
pleshd <pleskovd96@gmail.com>
2025-07-22 22:58:42 +0500

    Модель Project. Плешков

--- UP-05/Data/Models/Project.cs
+++ UP-05/Data/Models/Project.cs
@@ -2,6 +2,20 @@
 {
     public class Project
     {
-        
+        private int id { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private bool isPublic { get; set; }
+        public Project(Project project = null) 
+        {
+            if (project != null) 
+            {
+                this.id = project.id;
+                this.name = project.name;
+                this.description = project.description;
+                this.isPublic = project.isPublic;
+            }
+        }
+        public Project() { }
     }
 }
