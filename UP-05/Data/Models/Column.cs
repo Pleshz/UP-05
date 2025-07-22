57bfbab90773cb2dd3f88093f48e87085c0f0ea8
pleshd <pleskovd96@gmail.com>
2025-07-22 23:10:32 +0500

    Создал модель Column. Плешков

---
+++ UP-05/Data/Models/Column.cs
@@ -0,0 +1,16 @@
+﻿namespace UP_05.Data.Models
+{
+    public class Column
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private Project projectId { get; set; }
+
+        public Column(int id, string name, Project projectId)
+        {
+            this.id = id;
+            this.name = name;
+            this.projectId = projectId;
+        }
+    }
+}
