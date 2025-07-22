aef79f38538fb3e275e0c64e26aff75763942c40
pleshd <pleskovd96@gmail.com>
2025-07-22 23:25:10 +0500

    Создал модель Subtask. Плешков

---
+++ UP-05/Data/Models/Subtask.cs
@@ -0,0 +1,24 @@
+﻿namespace UP_05.Data.Models
+{
+    public class Subtask
+    {
+        private int id { get; set; }
+        private Task taskId { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private DateTime dueDate { get; set; }
+        private Column columnId { get; set; }
+        private TaskAssign taskAssignId { get; set; }
+
+        public Subtask(int id, Task taskId, string name, string description, DateTime dueDate, Column columnId, TaskAssign taskAssignId) 
+        {
+            this.id = id;
+            this.taskId = taskId;
+            this.name = name;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.columnId = columnId;
+            this.taskAssignId = taskAssignId;
+        }
+    }
+}
