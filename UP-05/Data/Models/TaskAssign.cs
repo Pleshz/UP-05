a5ed56ee181ee4867d90595de8d81207064a0bf4
pleshd <pleskovd96@gmail.com>
2025-07-22 23:20:39 +0500

    Создал модель TaskAssign. Плешков

---
+++ UP-05/Data/Models/TaskAssign.cs
@@ -0,0 +1,16 @@
+﻿namespace UP_05.Data.Models
+{
+    public class TaskAssign
+    {
+        private int id { get; set; }
+        private Task taskId { get; set; }
+        private User userId { get; set; }
+
+        public TaskAssign(int id, Task taskId, User userId) 
+        {
+            this.id = id;
+            this.taskId = taskId;
+            this.userId = userId;
+        }
+    }
+}
