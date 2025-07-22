a953307b07510e13683ec210fd035bcafb6af727
pleshd <pleskovd96@gmail.com>
2025-07-22 23:18:47 +0500

    Создал модель Task. Плешков

---
+++ UP-05/Data/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace UP_05.Data.Models
+{
+    public class Task
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private DateTime dueDate { get; set; }
+        private Column columnId { get; set; }
+
+        public Task(int id, string name, string description, DateTime dueDate, Column columnId) 
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.columnId = columnId;
+        }
+    }
+}
