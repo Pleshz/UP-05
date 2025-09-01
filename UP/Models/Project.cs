09769c7e81012a0c6e1dd2a672963f7d81008dbd
pleshz <pleskovd96@gmail.com>
2025-09-01 17:03:58 +0500

    Создал модель Project и Task. Плешков

---
+++ UP/Models/Project.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UP.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isPublic { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+
+        //Конструктор для создания нового проекта
+        public Project(string Name, string Description, bool isPublic, int CreatorId, DateTime CreatedAt, DateTime UpdateAt)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.isPublic = isPublic;
+            this.CreatorId = CreatorId;
+            this.CreatedAt = CreatedAt;
+            this.UpdateAt = UpdateAt;
+        }
+
+        //Конструктор для загрузки проекта из БД
+        public Project(int Id, string Name, string Description, bool isPublic, int CreatorId, DateTime CreatedAt, DateTime UpdateAt) {
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
+            this.isPublic = isPublic;
+            this.CreatorId = CreatorId;
+            this.CreatedAt = CreatedAt;
+            this.UpdateAt = UpdateAt;
+        }
+    }
+}
---
+++ UP/Models/Task.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ColumnId { get; set; }
+        public int ProjectId { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        //Конструктор для создания новой задачи
+        public Task(string Title, string Description, int ColumnId, int ProjectId, int CreatorId, DateTime CreatedAt, DateTime UpdatedAt, DateTime? DueDate = null)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.ColumnId = ColumnId;
+            this.ProjectId = ProjectId;
+            this.CreatorId = CreatorId;
+            this.CreatedAt = CreatedAt;
+            this.UpdatedAt = UpdatedAt;
+            this.DueDate = DueDate;
+        }
+
+        //Конструктор для загрузки задачи из БД
+        public Task(int Id, string Title, string Description, int ColumnId, int ProjectId, int CreatorId, DateTime CreatedAt, DateTime UpdatedAt, DateTime? DueDate) {
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description;
+            this.ColumnId = ColumnId;
+            this.ProjectId = ProjectId;
+            this.CreatorId = CreatorId;
+            this.CreatedAt = CreatedAt;
+            this.UpdatedAt = UpdatedAt;
+            this.DueDate = DueDate;
+        }
+    }
+}
