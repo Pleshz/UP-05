348210fd5a795ff3d5cb528b5fd9cf571e18ed8d
pleshz <pleskovd96@gmail.com>
2025-09-01 16:49:31 +0500

    Создал класс подключения и работы с БД. Создал модель User. Плешков

---
+++ UP/Classes/Common/Connection.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace UP.Classes.Common
+{
+    public class Connection
+    {
+        readonly static string path = "server=127.0.0.1;port=3306;database=UP;uid=root;";
+        public static MySqlConnection OpenConnection() {
+            MySqlConnection connection = new MySqlConnection(path);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(SQL, connection);
+            return command.ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
---
+++ UP/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UP.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Конструктор для создания нового пользователя
+        public User(string Login, string Email, string Password, DateTime CreatedAt, DateTime UpdateAt, string FullName = null, string Bio = null) {
+            this.Login = Login;
+            this.Email = Email;
+            this.Password = Password;
+            this.CreatedAt = CreatedAt;
+            this.UpdateAt = UpdateAt;
+            this.FullName = FullName;
+            this.Bio = Bio;
+        }
+
+        //Конструктор для загрузки пользователя из БД
+        public User(int Id, string Login, string Email, string Password, DateTime CreatedAt, DateTime UpdateAt, string FullName, string Bio)
+        {
+            this.Id = Id;
+            this.Login = Login;
+            this.Email = Email;
+            this.Password = Password;
+            this.CreatedAt = CreatedAt;
+            this.UpdateAt = UpdateAt;
+            this.FullName = FullName;
+            this.Bio = Bio;
+        }
+    }
+}
