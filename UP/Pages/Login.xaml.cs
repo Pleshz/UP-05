2bb41a33243894dc99ca889feb6540f8b911be25
Pleshz <pleshkovd96@gmail.com>
2025-09-28 15:37:26 +0500

    Метод авторизации. Плешков

--- UP/Pages/Login.xaml.cs
+++ UP/Pages/Login.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UP.Classes.Context;
 
 namespace UP.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Login.xaml
-    /// </summary>
     public partial class Login : Page
     {
         public Login()
         {
             InitializeComponent();
+        }
+
+        public void LoginBtn(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, заполните логин и пароль.");
+                return;
+            }
+
+            UserContext userContext = new UserContext();
+            var allUsers = userContext.AllUsers();
+
+            var user = allUsers.FirstOrDefault(u => u.Login == login && u.Password == password);
+
+            if (user == null)
+            {
+                MessageBox.Show("Неверный логин или пароль.");
+                return;
+            }
+
+            MainWindow.init.OpenPages(MainWindow.pages.profile);
         }
     }
 }
