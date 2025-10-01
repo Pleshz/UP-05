ed6583cac778548b521256bfaba95e360f922d51
Pleshz <pleskovd96@gmail.com>
2025-10-01 10:25:01 +0500

    Допроверка. Плешков

---
+++ UP/Elements/SubtaskItem.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using UP.Classes.Context;
+
+namespace UP.Elements
+{
+    public partial class SubtaskItem : UserControl
+    {
+        public SubtaskItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is SubtaskContext subtask)
+            {
+                MainWindow.CurrentSubtask = subtask;
+                MainWindow.init.OpenPages(MainWindow.pages.subtaskManage);
+            }
+        }
+    }
+}
